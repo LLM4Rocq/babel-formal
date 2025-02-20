5c4737e0780c4c12623ec8ff7c6aded55498e52c
theostos <theo.stoskopf@proton.me>
2025-02-20 20:24:50 +0100

    Step 0 bis: Filter terms based on compilation success (Petanque) using parsed steps and extracted source file

---
+++ src/steps/step_0_bis/exec.py
@@ -0,0 +1,57 @@
+import os
+import argparse
+import json
+import re
+import shutil
+import logging
+logger = logging.getLogger(__name__)
+
+from tqdm import tqdm
+from src.training.eval import eval_tactics
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', default='export/step_0/', help='Output path from previous step')
+    parser.add_argument('--output', default='export/step_0_bis/', help='Output path')
+    args = parser.parse_args()
+
+    if not os.path.exists(args.output):
+        shutil.copytree(args.input, args.output, dirs_exist_ok=False)
+
+    pattern = r'(term_[0-9]+).json'
+    to_do = []
+    for root, subdirs, files in os.walk(args.output):
+        for filename in files:
+            match = re.match(pattern, filename)
+            if match:
+                data_path = os.path.join(root, filename)
+                source_path = os.path.join(root, 'source.v')
+                to_do.append((data_path, source_path))
+
+    for data_path, source_path in tqdm(to_do):
+        with open(data_path, 'r') as file_io:
+            data = json.load(file_io)
+        
+        if 'pytanque_check' in data:
+            continue
+        name_thm = data['name']
+        tactics = data['steps']
+        
+        goal_init, res = eval_tactics(name_thm, source_path, tactics)
+
+        if res[-1]['status'] != 'finish':
+            logger.warning(f'{data_path} does not compile using Pytanque. Ignore the file.')
+            logger.warning(f'Last result: {res[-1]}')
+            os.remove(data_path)
+            continue
+        
+        if 'coqpyt_aux' in data['term']:
+            logger.warning(f'{data_path} term seems to contains trace of partial compilation.')
+            os.remove(data_path)
+            continue
+
+        data['goals'] = [goal_init] + [entry['goals'] for entry in res]
+        data['pytanque_check'] = True
+        with open(data_path, 'w') as file_io:
+            json.dump(data, file_io, indent=4)
+        
